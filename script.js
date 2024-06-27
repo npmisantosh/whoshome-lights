18fe0ebceeedefecc26d4f0ef0bab6e39f0ba09e
Santosh Humagain <santoshhumagain1997@gmail.com>
2024-06-27 14:49:04 -0400

    some changes

--- script.js
+++ script.js
@@ -10,8 +10,21 @@ function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
-  console.log(ev.toElement.id);
-  checkwhoisinroom();
-}
 
-function checkwhoisinroom() {}
+  if (ev.toElement.children.length != 2) {
+    if (ev.srcElement.lastChild.id == "her") {
+      console.log(ev.toElement.id + " usko lights");
+      //usko lights
+      ev.toElement.classList.add("her");
+      console.log(ev.srcElement.classList);
+    }
+
+    if (ev.srcElement.lastChild.id == "me") {
+      ev.toElement.classList.add("me");
+      console.log(ev.toElement.id + " mero lights");
+    }
+  } else {
+    ev.toElement.classList.add("her");
+    console.log(ev.toElement.id + " usko lights");
+  }
+}
